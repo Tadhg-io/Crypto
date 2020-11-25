99085867ba95877beaad8fc983c1a358a3e0904b
Tadhg <oconghat@tcd.ie>
2020-11-25 21:49:52 +0000

    React BrowserRouter for navigating the frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 class App extends Component {
@@ -24,12 +24,12 @@ class App extends Component {
                     Sample blockchain.
                 </div>
                 <br />
+                <div><Link to='/blocks'>Blocks</Link></div>
+                <br />
                 <div className='WalletInfo'>
                     <div>Address: {address}</div>
                     <div>Balance: {balance}</div>
                 </div>
-                <br />
-                <Blocks />
             </div>
         );
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import history from './history'
 import App from './components/App';
+import Blocks from './components/Blocks'
 import './index.css';
 
 render(
-    <App />, 
+    <BrowserRouter history={history}>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route exact path='/blocks' component={Blocks} />
+        </Switch>
+    </BrowserRouter>, 
     document.getElementById('root')
 );
